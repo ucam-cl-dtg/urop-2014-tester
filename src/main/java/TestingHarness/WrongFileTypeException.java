c55dea943cb38cf397ecaae9dc74d27e7fc7d2a6
Alexander Simpson <as2388@cam.ac.uk>
2014-07-11 13:22:31 +0100

    More JavaDoc

--- src/main/java/TestingHarness/WrongFileTypeException.java
+++ src/main/java/TestingHarness/WrongFileTypeException.java
@@ -1,7 +1,6 @@
 package TestingHarness;
 
-public class WrongFileTypeException extends Exception {
-	
+public class WrongFileTypeException extends Exception {	
 	public WrongFileTypeException() {
 		super("Error: Test files must end in .xml or .java only");
 	}
