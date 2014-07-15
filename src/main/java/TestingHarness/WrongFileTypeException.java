941df3cc63ece92b7f3bcd62b77bb528d2642768
Alexander Simpson <as2388@cam.ac.uk>
2014-07-15 11:23:02 +0100

    Added JavaDoc

--- src/main/java/TestingHarness/WrongFileTypeException.java
+++ src/main/java/TestingHarness/WrongFileTypeException.java
@@ -1,5 +1,11 @@
 package TestingHarness;
 
+/**
+ * Should be thrown when the provided list of files containing the tests to be 
+ * run contains a file which is not a .xml (for static analysis) or 
+ * .java (for dynamic analysis)
+ * @author kls2510
+ */
 public class WrongFileTypeException extends Exception {	
 	public WrongFileTypeException() {
 		super("Error: Test files must end in .xml or .java only");
