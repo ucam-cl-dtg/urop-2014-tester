5effc89c55b2ebf3943324abb59ec80fc5ce33cc
Alexander Simpson <as2388@cam.ac.uk>
2014-07-17 14:45:31 +0100

    Added friendly, readable messages to CheckstyleFormat.xml for naming convention violations

---
+++ src/test/resources/NamingInvalidConstant.java
@@ -0,0 +1,6 @@
+//test case for constants not containing only upper case letters and numbers,
+//where a java constant is a static final field
+public class NamingInvalidConstant
+{
+    private static final int test;
+}
---
+++ src/test/resources/NamingInvalidMember.java
@@ -0,0 +1,5 @@
+//test case for members not of form ^[a-z][a-zA-Z0-9]*$
+public class NamingInvalidMember
+{
+    private int TestCase;
+}
