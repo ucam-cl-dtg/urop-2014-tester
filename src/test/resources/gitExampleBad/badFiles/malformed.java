36db7dd138eff8def3b585de8fd996a7914ba0b0
Alexander Simpson <as2388@cam.ac.uk>
2014-07-21 16:19:12 +0100

    Added comment to badFiles/malformed.java

--- src/test/resources/gitExampleBad/badFiles/malformed.java
+++ src/test/resources/gitExampleBad/badFiles/malformed.java
@@ -4,4 +4,8 @@ public class malformed {
     private int ExampleVar;
     public void test() {
         
+    //Note the missing '}' here.
+    /* Once dynamic tests are working, we may not need to worry about this kind of case,
+     * as the dynamic tests  would fail first, but it's included for now just in case
+     */
 }
