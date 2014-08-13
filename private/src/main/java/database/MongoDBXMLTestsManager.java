3976bd3418ab0186376f902c3e14634c3ee02c7b
Alexander Simpson <as2388@cam.ac.uk>
2014-08-13 17:14:33 +0100

    Added logs to MongoDB manager classes

--- private/src/main/java/database/MongoDBXMLTestsManager.java
+++ private/src/main/java/database/MongoDBXMLTestsManager.java
@@ -40,51 +40,57 @@ public class MongoDBXMLTestsManager implements IDBXMLTestsManager {
     /** {@inheritDoc} */
 	@Override
 	public void addNewTest(String tickId, List<StaticOptions> staticTestSettings) throws TestIDAlreadyExistsException {
+        log.debug("Saving new stylistic settings for tickId " + tickId);
+
         //if XMLTestSettings for the given tickId are already in the database, throw an exception
         if (XMLTestSettingsColl.findOneById(tickId) != null) {
+            log.warn("Stylistic settings for tickId " + tickId + " already exist in the database. Throwing TestIDAlreadyExistsException");
             throw new TestIDAlreadyExistsException(tickId);
         }
 
         XMLTestSettingsColl.save(new ListWrapper(tickId, staticTestSettings));
+        log.debug("Stylistic settings saved for tickId " + tickId);
 	}
 
     /** {@inheritDoc} */
     @Override
     public void update(@PathParam("tickId") String tickId, List<StaticOptions> staticTestSettings) throws TestIDNotFoundException {
+        log.debug("Updating stylistic settings in db for tickId " + tickId);
         if (XMLTestSettingsColl.findOneById(tickId) == null) {
+            log.warn("tickId " + tickId + " not found in the database; throwing TestIDNotFoundException");
             throw new TestIDNotFoundException(tickId);
         }
 
-        //hopefully this overwrites automatically
-        XMLTestSettingsColl.save(new ListWrapper(tickId,staticTestSettings));
-        
-        /* deleteTest(tickId);
-        try {
-            addNewTest(tickId, staticTestSettings);
-        }
-        catch (TestIDAlreadyExistsException e) {
-            //because addNewTest() is preceded by deleteTest(), this should not happen
-        } */
+        XMLTestSettingsColl.save(new ListWrapper(tickId, staticTestSettings));
+
+        log.debug("Stylistic settings for tickId " + tickId + " updated, returning...");
     }
 
     /** {@inheritDoc} */
 	@Override
 	public void deleteTest(String tickId) throws TestIDNotFoundException {
+        log.debug("Deleting stylistic settings from db for tickId " + tickId);
         if (XMLTestSettingsColl.findOneById(tickId) == null) {
+            log.warn("tickId " + tickId + " not found in database; throwing TestIDNotFoundException");
             throw new TestIDNotFoundException(tickId);
         }
 
         XMLTestSettingsColl.removeById(tickId);
+        log.debug("Removed stylistic settings for tickId " + tickId + "; returning...");
 	}
 
     /** (@inheritDoc} */
 	@Override
 	public List<StaticOptions> getTestSettings(String tickId) throws TestIDNotFoundException {
+        log.debug("Obtaining stylistic settings from db for tickId " + tickId);
         if (XMLTestSettingsColl.findOneById(tickId) == null) {
+            log.warn("tickId " + tickId + " not found in database; throwing TestIDNotFoundException");
             throw new TestIDNotFoundException(tickId);
         }
 
         ListWrapper listWrapper = XMLTestSettingsColl.findOneById(tickId);
+
+        log.debug("Stylistic settings found for tickId " + tickId + "; returning...");
         return listWrapper.getElements();
 	}
 }
